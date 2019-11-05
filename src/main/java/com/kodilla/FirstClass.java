0590eae40a17a16322f88f4d7d46c7d89be4f57a
Tomek <abelectomasz@gmail.com>
2019-11-05 21:18:04 +0100

    modul kodilla-testing

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -1,4 +1,9 @@
 package com.kodilla;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
 public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("daksdasjdlajdljasl");
+    }
 }
