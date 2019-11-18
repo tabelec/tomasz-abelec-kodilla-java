b94adc50a2565a2c31b5806c5eba3c44bcc408da
Tomek <abelectomasz@gmail.com>
2019-11-19 00:19:21 +0100

    zadanie 7.3 pop1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,15 +3,18 @@ package com.kodilla.stream;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
         Forum theForum = new Forum();
+
         Map<String, ForumUser> theResultMapOfUsers = theForum.getList().stream()
                 .filter(forum -> forum.getGender() == 'M')
-                .filter(forum -> forum.getBirthDate().getYear() <= 1999)
+                .filter(forum -> Period.between(forum.getBirthDate(), LocalDate.now()).getYears() > 20)
                 .filter(forum -> forum.getPostCount() > 0)
                 .collect(Collectors.toMap(ForumUser::getUserID, ForumUser -> ForumUser));
 
